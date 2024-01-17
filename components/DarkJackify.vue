10a99d5550535883f91961ce0614ecde0884eb81
jkpogchamp <jkpogchamper@gmail.com>
2024-01-17 11:21:28 +0100

    add canvas, scratch animation and cursor image

--- components/DarkJackify.vue
+++ components/DarkJackify.vue
@@ -1,33 +1,117 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
+import poszto from '@/assets/images/poszto.svg'
 
 export default defineComponent({
   name: "DarkJackify",
+  mounted() {
+    this.initCanvas()
+  },
+  methods: {
+    initCanvas() {
+      const canvas = document.getElementById('js-canvas') as HTMLCanvasElement
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+      const img = new Image()
+      img.src = poszto
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0)
+      }
+
+      let mouseX = 0
+      let mouseY = 0
+      let isDragged = false
+
+      let events = {
+        mouse: {
+          down: 'mousedown',
+          up: 'mouseup',
+          move: 'mousemove',
+        },
+        touch: {
+          down: 'touchstart',
+          up: 'touchend',
+          move: 'touchmove',
+        },
+      }
+
+      let deviceType = ""
+
+      const isTouchDevice = () => {
+        try {
+          document.createEvent("TouchEvent");
+          deviceType = "touch"
+          return true;
+        } catch (e) {
+          deviceType = "mouse"
+          return false;
+        }
+      }
+
+      let rectLeft = canvas.getBoundingClientRect().left
+      let rectTop = canvas.getBoundingClientRect().top
+
+      const getXY = (e) => {
+        mouseX = (!isTouchDevice() ? e.pageX : e.touches[0].pageX) - rectLeft
+        mouseY = (!isTouchDevice() ? e.pageY : e.touches[0].pageY) - rectTop
+      }
+      const scratch = (x, y)=> {
+        ctx.globalCompositeOperation = 'destination-out'
+        ctx.beginPath()
+        ctx.arc(x, y, 32, 0,  2*Math.PI,)
+        ctx.fill();
+      }
+
+      isTouchDevice()
+      canvas.addEventListener(events[deviceType].down, (e) => {
+        isDragged = true
+        getXY(e)
+        scratch(mouseX, mouseY)
+      })
+
+      canvas.addEventListener(events[deviceType].move, (e) => {
+        if (!isTouchDevice()) {
+          e.preventDefault();
+        }
+        if (isDragged) {
+          getXY(e)
+          scratch(mouseX, mouseY)
+        }
+      })
+
+      canvas.addEventListener(events[deviceType].up, () => {
+        isDragged = false
+      })
+
+      canvas.addEventListener('mouseleave', () => {
+        isDragged = false
+      })
+    }
+  },
 })
 </script>
 
 <template>
   <section class="dark-jackify-wrapper" id="container">
-    <canvas class="canvas" id="js-canvas" width="442" />
     <div class="scratch">
       <BlackJack />
       <BonusGame />
     </div>
-
+    <canvas class="canvas" id="js-canvas" height="380" width="392"/>
   </section>
 </template>
 
 <style scoped>
+  .canvas {
+    position: absolute;
+    top: 326px;
+    left: 24px;
+    cursor: url("@/assets/images/kaparoeszkoz.svg") 32 32, grab;
+  }
+
   .dark-jackify-wrapper {
     position: relative;
     height: 782px;
     width: 442px;
     background: url('@/assets/images/lekapart.svg');
   }
-
-  .canvas {
-    position: absolute;
-    top: 0;
-  }
-
 </style>
